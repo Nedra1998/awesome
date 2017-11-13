8e8e41c331b1126138ac0479109c7c494bea141c
Arden Rasmussen <ardenisthebest@gmail.com>
2017-11-13 11:14:09 -0800

    Began major rewrite

---
+++ rc.lua
@@ -0,0 +1,20 @@
+-------------------------------------------------------------------------------
+--                                Base Config                                --
+-------------------------------------------------------------------------------
+
+-- Loading Modules: {{{
+-------------------------------------------------------------------------------
+
+
+-- Standard Awesome Libraries
+---------------------------------------
+local awful = require("awful")
+local wibox = require("wibox")
+local beautiful = require("beautiful")
+require("awful.autofocus")
+require("debian.menu")
+
+-- Error Handling
+-------------------------------------------------------------------------------
+require("modules.ercheck")
+
